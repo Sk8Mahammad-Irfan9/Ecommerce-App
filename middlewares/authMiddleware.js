b1d2aaeffc00bf58c4ac8e95e484dcebddef85e7
Sk Mahammad Irfan <109087483+Sk8Mahammad-Irfan9@users.noreply.github.com>
2024-03-26 09:48:13 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -14,27 +14,6 @@ exports.requireSignIn = async (req, res, next) => {
   }
 };
 
-// export const isAdmin = async (req, res, next) => {
-//   try {
-//     const user = await userModel.findById(req.user._id);
-//     if (user.role !== 1) {
-//       return res.status(401).send({
-//         success: false,
-//         message: "UnAuthorized Access",
-//       });
-//     } else {
-//       next();
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(401).send({
-//       success: false,
-//       error,
-//       message: "Error in admin middelware",
-//     });
-//   }
-// };
-
 exports.isAdmin = async (req, res, next) => {
   try {
     const user = await userModel.findById(req.user._id);
