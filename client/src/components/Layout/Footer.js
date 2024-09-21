1b06562b8954e1e2be53e43352f815aab0ae4e85
Sk Mahammad Irfan <109087483+Sk8Mahammad-Irfan9@users.noreply.github.com>
2024-09-21 20:44:54 +0530

    footer update

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1 className="text-center">Footer component &copy;</h1>
+      <h1 className="text-center">Copyright &copy; 2022 All rights reserved</h1>
+      <p style={{ textAlign: "center" }}>
+        Website developed by <i>Sk Mahammad Irfan</i>
+      </p>
       <p className="text-center mt-3">
-        <Link to='/about'>About</Link>|
-        <Link to='/contact'>Contact</Link>|
-        <Link to='/policy'>Privacy Policy</Link>|
+        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>|
       </p>
     </div>
   );
